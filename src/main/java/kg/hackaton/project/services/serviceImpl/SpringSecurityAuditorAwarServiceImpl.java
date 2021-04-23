60a8ed96f7c731899a544c533a68ead9b1e8bd0d
AbdulloKarimov <abdullokh2020@gmail.com>
2021-04-23 18:39:00 +0600

    added audit

---
+++ src/main/java/kg/hackaton/project/services/serviceImpl/SpringSecurityAuditorAwarServiceImpl.java
@@ -0,0 +1,23 @@
+package kg.hackaton.project.services.serviceImpl;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public class SpringSecurityAuditorAwarServiceImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+
+        return Optional.ofNullable(((UserDetails) authentication.getPrincipal()).getUsername());
+
+    }
+}
